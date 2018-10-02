eaf113dceaa47c5646a56627f6748b212f681ff8
marcelo.bhegedus <marceloborellihegedus@gmail.com>
2018-10-02 20:30:34 -0300

    Adicionadio classe database

---
+++ PI4/src/main/java/com/mycompany/pi4/Database.java
@@ -0,0 +1,23 @@
+package com.mycompany.pi4;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Database {
+    
+    private static Database instance = null;
+    
+    private Database(){};
+    
+    public static Database get () {
+        if(instance == null)
+            instance = new Database();
+        return instance;
+    }
+    
+    public Connection conn () throws Exception {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        Connection conn = DriverManager.getConnection("jdbc:sqlserver://pijulio.database.windows.net:1433;user=julio@pijulio;password=Abcd123!;database=facenac");
+        return conn;
+    }
+}
