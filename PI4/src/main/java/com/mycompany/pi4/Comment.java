078a41d79ecccee4bd5cd0411c29114aa9cc3927
Julio Klein <julio.ckcalista@gmail.com>
2018-11-08 21:27:12 -0200

    ...

---
+++ PI4/src/main/java/com/mycompany/pi4/Comment.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pi4;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author julio.ckcalista
+ */
+@XmlRootElement
+public class Comment implements Serializable {
+    private String comment;
+    private int id, usuario, historia;
+    private Date data;
+
+    public Comment(String comment, int id, int usuario, int historia, Date data) {
+        this.comment = comment;
+        this.id = id;
+        this.usuario = usuario;
+        this.historia = historia;
+        this.data = data;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(int usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getHistoria() {
+        return historia;
+    }
+
+    public void setHistoria(int historia) {
+        this.historia = historia;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+    
+    
+}
